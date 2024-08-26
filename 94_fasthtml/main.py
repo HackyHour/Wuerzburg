3eb5abd748631cec1677f6a0d676885cec27ef38
Markus Ankenbrand <markus@ankenbrand.me>
2024-08-26 20:48:27 +0200

    added check

--- 94_fasthtml/main.py
+++ 94_fasthtml/main.py
@@ -17,10 +17,13 @@ m2 = img[:, :, 1] == 0
 intersect = np.logical_and(m1, m2)
 dice = 2*np.sum(intersect) / (np.sum(m1) + np.sum(m2))
 
+
+gguess = 50
+
 app, rt = fast_app()
 
 @matplotlib2fasthtml
-def generate_chart(num_points):
+def generate_chart():
     # plotdata = [np.random.exponential(1) for _ in range(num_points)]
     #plt.plot(range(len(plotdata)), plotdata)
     plt.imshow(img)
@@ -29,20 +32,45 @@ def generate_chart(num_points):
 def homepage():
     return Div(
         Div("Fill me by clicking the button below", id="chart"),
-        H3("Move the slider to change the graph"),
-        Button(
-            "Click me !",
+	H3("Move the slider to change the graph"),
+	Input(name="guess", type="range", min="0", max="100", value="50", get=slider_value, hx_target="#slider"),
+	H3(id="slider"),
+	Div(id="check_div"),
+	Div(Button(
+            "Check",
+            # type="range",
+            # min="1", max="10", value="1",
+            get=check, hx_target="#check_div",
+            name='button'),
+	Button(
+            "generate",
             # type="range",
             # min="1", max="10", value="1",
             get=update_chart, hx_target="#chart",
-            name='slider')
+            name='button')),
     )
 
+@app.get("/slider_value")
+def slider_value(guess: int):
+    global gguess
+    gguess = guess
+    return H3(f"Slider value: {guess/100}")
+
 @app.get("/update_charts")
-def update_chart(slider: int):
+def update_chart():
     return Div(
-        generate_chart(slider),
-        P(f"Dice: {dice}")
+        generate_chart(),
+        #P(f"Dice: {dice}"),
+        #P(f"You are {np.abs(dice*100 - gguess)}% off")
 )
+
+@app.get("/check")
+def check():
+    off = np.abs(dice.round(2)*100 - gguess)
+    return Div(
+        P(f"Dice: {dice.round(2)}"),
+        P(f"You are {off.round(2)}% off")
+)
+
 
 serve()
